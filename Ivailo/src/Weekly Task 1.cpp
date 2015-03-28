8473b21b81d6fa3a17e4c1bdcdf44cf5b0860b5d
Oxidro <ivaylo.ivanov.95@hotmai.com>
2015-03-28 02:16:25 +0200

    Almost done

--- Ivailo/src/Weekly Task 1.cpp
+++ Ivailo/src/Weekly Task 1.cpp
@@ -17,7 +17,7 @@ int main() {
 	squirrel2.rePaint("white");
 	squirrel1.compare(squirrel2);
 	cout<<endl<<"End of class Squirrel test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Warrior ... "<<endl<<endl;
 	Warrior warrior1(100, 20, 5, 10), warrior2(70, 50, 0, 20);
 	warrior1.print(); warrior2.print();
@@ -25,7 +25,7 @@ int main() {
 	warrior2.spellForReducingStrenght(warrior1);
 	warrior1.print(); warrior2.print();
 	cout<<endl<<"End of class Warrior test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Box ... "<<endl<<endl;
 	Box box(0,2);
 	box.print();
@@ -36,7 +36,7 @@ int main() {
 	box.increaseSize();
 	box.print();
 	cout<<endl<<"End of class Box test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Concert ... "<<endl<<endl;
 	Concert concert("Earth_sounds", 19.25, "Hall-Bulgaria", 250);
 	concert.print();
@@ -44,7 +44,7 @@ int main() {
 	concert.delay(20.15);
 	concert.print();
 	cout<<endl<<"End of class Concert test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Baloon ... "<<endl<<endl;
 	Baloon baloon("standart_cylindrical", 0, 1);
 	baloon.inflate();
@@ -56,7 +56,7 @@ int main() {
 	baloon.print();
 	baloon.inflate();
 	cout<<endl<<"End of class Baloon test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Plant ... "<<endl<<endl;
 	Plant plant(1, 0, 0 ,5 ,0);
 	plant.addYears(3);
@@ -65,7 +65,7 @@ int main() {
 	plant.addYears(5);
 	plant.print();
 	cout<<endl<<"End of class Plant test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Product ... "<<endl<<endl;
 	Product product(0.70, 1000, "banana");
 	product.print();
@@ -73,7 +73,7 @@ int main() {
 	product.itemsBought(275);
 	product.print();
 	cout<<endl<<"End of class Product test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Bacteria ... "<<endl<<endl;
 	Bacteria b("Lactobacillus_Bulgaricus", 5, 10, 'G', 1);
 	b.passedHours(5);
@@ -82,7 +82,7 @@ int main() {
 	b.passedHours(5);
 	b.print();
 	cout<<endl<<"End of class Bacteria test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Car ... "<<endl<<endl;
 	Car c1, c2;
 	for(int i=0; i<=4; i++)
@@ -93,7 +93,7 @@ int main() {
 	c1.printCar();
 	c2.printCar();
 	cout<<endl<<"End of class Car test ..."<<endl<<endl;
-	//-----------------------------------------------------------------------------
+	//------------------------------------------------------------------------------
 	cout<<"Testing Tail ... "<<endl<<endl;
 	Tail t;
 	for(int i=0; i<=15; i++)

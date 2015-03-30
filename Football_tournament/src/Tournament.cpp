d744c9b9da2144d39c4f71a0d25a207088362809
DenislavParvanov <denislav.parvanov@gmail.com>
2015-03-30 11:07:56 +0300

    add tournament class

---
+++ Football_tournament/src/Tournament.cpp
@@ -0,0 +1,21 @@
+/*
+ * Tournament.cpp
+ *
+ *  Created on: 30.03.2015 ã.
+ *      Author: Deni
+ */
+
+#include "Tournament.h"
+
+namespace std {
+
+Tournament::Tournament() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Tournament::~Tournament() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ Football_tournament/src/Tournament.h
@@ -0,0 +1,21 @@
+/*
+ * Tournament.h
+ *
+ *  Created on: 30.03.2015 ã.
+ *      Author: Deni
+ */
+
+#ifndef TOURNAMENT_H_
+#define TOURNAMENT_H_
+
+namespace std {
+
+class Tournament {
+public:
+	Tournament();
+	virtual ~Tournament();
+};
+
+} /* namespace std */
+
+#endif /* TOURNAMENT_H_ */

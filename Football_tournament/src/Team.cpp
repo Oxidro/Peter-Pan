1e692b94dd2acda94112fb1dba8ab6258a9c8be1
DenislavParvanov <denislav.parvanov@gmail.com>
2015-05-14 12:54:38 +0300

    Remove unused staff from Team

--- Football_tournament/src/Team.cpp
+++ Football_tournament/src/Team.cpp
@@ -6,112 +6,95 @@
  */
 #include<iostream>
 #include"string.h"
-#include"ClassTeam.h"
+#include"Team.h"
 using namespace std;
 
-void shortName(char c)
-{
-	if(c>=97 || c<=122)
-		cout<<c-32;
+void shortName(char c) {
+	if (c >= 97 || c <= 122)
+		cout << c - 32;
 	else
-		cout<<c;
+		cout << c;
 }
 
-void printStr(char *str)
-{
+void printStr(char *str) {
 	char* pointerToStr = str;
-	while (*pointerToStr)
-	{
+	while (*pointerToStr) {
 		cout << *pointerToStr++;
 	}
 }
-Team::Team()
-{
-	teamName=new char[51];
-	group=0;
-	won=draw=lost=goalDifference=totalPoints=0;
-	top=-1;
+Team::Team() {
+	teamName = new char[51];
+
 }
 
-void Team::setteamName(char* _teamName)
-{
-	int len=strlen(_teamName);
+void Team::setteamName(char* _teamName) {
+	int len = strlen(_teamName);
 	delete[] teamName;
-	teamName = new char[len+1];
+	teamName = new char[len + 1];
 	strcpy(teamName, _teamName);
-	teamName[len+1] = '\0';
+	teamName[len + 1] = '\0';
 }
 
-Team::Team(const Team& t)
-	: teamName(NULL), group(t.group), won(t.won), draw(t.draw), lost(t.lost), goalDifference(t.goalDifference), totalPoints(t.totalPoints), top(t.top)
-{
+Team::Team(const Team& t) :
+		teamName(NULL) {
 	setteamName(t.teamName);
 }
 
-Team& Team :: operator=(const Team& t)
-{
-	if(this ==	&t)
+Team& Team::operator=(const Team& t) {
+	if (this == &t)
 		return *this;
-	group=t.group;
-	won=t.won;
-	draw=t.draw;
-	lost=t.lost;
-	goalDifference=t.goalDifference;
-	totalPoints=t.totalPoints;
-	top=t.top;
+
 	setteamName(t.teamName);
 	return *this;
 }
 
-Team::~Team()
-{
+Team::~Team() {
 	delete teamName;
 }
 
 /*
-void Player::print()
-{
-	cout<<"Name: ";
-	printStr(name);
-}
+ void Player::print()
+ {
+ cout<<"Name: ";
+ printStr(name);
+ }
 
-void Team::addPlayer(Player *p)
-{
-	players[++top]=p;
-}
+ void Team::addPlayer(Player *p)
+ {
+ players[++top]=p;
+ }
 
-void Team::removePlayer(Player *p)
-{
-	for(int i=0; i<=top; i++)
-	{
-		if(p==players[i])
-		{
-			i++;
-			players[i-1]=players[i];
-		}
-	}
-	top--;
-}
-void Team::printTeam()
-{
-	cout<<"Team name: "<<teamName<<endl;
-	cout<<"Group: "<<group<<endl;
-	cout<<"Players: "<<endl;
-	for(int i=0; i<=top; i++)
-	{
-		printStr(players[i]->getName());
-	}
-}
-void Team::printTeamStatistics()
-{
-	cout<<"Team name W   D   L   GD   Pts";
-	for(int i=0; i<3; i++)
-	{
-	shortName(teamName[i]);
-	}
-	cout<<won<<"       "<<draw<<"   "<<lost<<"   "<<goalDifference<<"   "<<totalPoints;
+ void Team::removePlayer(Player *p)
+ {
+ for(int i=0; i<=top; i++)
+ {
+ if(p==players[i])
+ {
+ i++;
+ players[i-1]=players[i];
+ }
+ }
+ top--;
+ }
+ void Team::printTeam()
+ {
+ cout<<"Team name: "<<teamName<<endl;
+ cout<<"Group: "<<group<<endl;
+ cout<<"Players: "<<endl;
+ for(int i=0; i<=top; i++)
+ {
+ printStr(players[i]->getName());
+ }
+ }
+ void Team::printTeamStatistics()
+ {
+ cout<<"Team name W   D   L   GD   Pts";
+ for(int i=0; i<3; i++)
+ {
+ shortName(teamName[i]);
+ }
+ cout<<won<<"       "<<draw<<"   "<<lost<<"   "<<goalDifference<<"   "<<totalPoints;
 
-}
-*/
-
+ }
+ */
 
--- Football_tournament/src/Team.h
+++ Football_tournament/src/Team.h
@@ -8,17 +8,14 @@
 #ifndef CLASSTEAM_H_
 #define CLASSTEAM_H_
 
-
-
-class Team
-{
+class Team {
 private:
 	char* teamName;
 	Player* players[18];
 	/*
-	char group;
-	int won, draw, lost, goalDifference, totalPoints, top;
-	*/
+	 char group;
+	 int won, draw, lost, goalDifference, totalPoints, top;
+	 */
 
 public:
 	// Default constructor
@@ -30,28 +27,21 @@ public:
 	// Destructor
 	~Team();
 
-	char* getTeamName(){return teamName;};
-	char getGroup(){return group;};
+	char* getTeamName() {
+		return teamName;
+	}
+	;
 
-	int getWon(){return won;};
-	int getDraw(){return draw;};
-	int getLost(){return lost;};
-	int getGoalDifferece(){return goalDifference;};
-	int getTotalPoints(){return totalPoints;};
-
-	Team* getTeam(){return this;};
+	Team* getTeam() {
+		return this;
+	}
+	;
 
 	void setteamName(char* _teamName);
-	void setGroup(char _group){group=_group;};
-	void setWor(int _won){won=_won;};
-	void setDraw(int _draw){draw=_draw;};
-	void setLost(int _lost){lost=_lost;};
-	void setGoalDifference(int _goalDifference){goalDifference=_goalDifference;};
-	void setTotalPoints(int _totalPoints){totalPoints=_totalPoints;};
 
-/*	void addPlayer(Player*);
-	void removePlayer(Player*);
-*/
+	/*	void addPlayer(Player*);
+	 void removePlayer(Player*);
+	 */
 	void printTeam();
 	void printTeamStatistics();
 };

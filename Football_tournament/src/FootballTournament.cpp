79511c0705b27daac316579d9b2e620282829c6b
StelaN <stelanovachkova@gmail.com>
2015-06-01 00:35:42 +0300

    something like a menu

--- Football_tournament/src/FootballTournament.cpp
+++ Football_tournament/src/FootballTournament.cpp
@@ -7,6 +7,8 @@
 //============================================================================
 
 #include <iostream>
+using namespace std;
+#include <cstring>
 
 #include "Match.h"
 #include "Event.h"
@@ -23,12 +25,32 @@ using namespace std;
 
 int main()
 {
-	Player players1[18];
+    cout<<"Welcome to the football match simulaor!\n";
+    cout<<"Please enter the names of the teams in the match.\nTeam one:";
+    string name1, name2;
+    getline(cin,name1);
+    cout<<"Team two:";
+    getline(cin,name2);
+
+
+    //reading the players from file for easier input
+    //...
+    Player players1[18];
 	Player players2[18];
-	Team team1("Name1", players1);
-	Team team2("Name2", players2);
-
-	Match match(team1, team2);
-
+    Team team1(name1, players1);
+    Team team2(name2, players2);
+    Match match(team1, team2);
+    match.setStartPosition(team1,team2);
+    Event game;
+    cout<<"Let the match begin!\n";
+    match.playMatch();
+    cout<<game<<endl;
+    cout<<team1.getTeamName()<<"   "<<game.goalsOfTeamA<<" : "<<game.goalsOfTeamB<<"   "<<team2.getTeamName()<<endl<<endl;
+    cout<<"The winner is ";
+    if(game.goalsOfTeamA>game.goalsOfTeamB){
+        cout<<"The winner is "<<team1.getTeamName()<<"!"<<endl;
+    } else if(game.goalsOfTeamA<game.goalsOfTeamB){
+            cout<<"The winner is "<<team2.getTeamName()<<"!"<<endl;
+        } else cout<<team1.getTeamName()<<" and "<<team2.getTeamName()<<" ended in a draw!"<<endl;
 	return 0;
 }
